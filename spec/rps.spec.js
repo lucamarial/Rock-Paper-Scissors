303d5cdd80c8c597f1d3eacf6978561fc51ba30f
luca <l.lobacher@web.de>
2019-09-30 14:32:01 +0200

    Adds smoke unit test if RockPaperScissors function exists

--- spec/rps.spec.js
+++ spec/rps.spec.js
@@ -1,18 +1,14 @@
 require('../spec.helper')
 
-describe('Your test case description', () => {
-    // Setup
-    let array
+describe('Rock-Paper-Scissor Game', () => {
 
-    beforeEach(() => {
-        // assign values to your variables
-        array = new Array(2, 3)
-    });
+    describe('smoke tests', () => {
+        it('should exist', () => {
+            expect(RockPaperScissor).to.exist;
+        })
 
-    it('add a descriptive test title', () => {
-        // Execute code if needed
-        const sum = array[0] + array[1]
-        // add an assertion using the `expect` keyword
-        expect(sum).to.eql(5)
+        it('should be a function', () => {
+            expect(RockPaperScissor).to.be.a('function');
+        })
     })
 })
